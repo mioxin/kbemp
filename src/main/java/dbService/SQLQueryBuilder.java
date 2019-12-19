af850ef28a64429409775434ce1a9214e8006a3b
M. Palchuk <mrmioxin@gmail.comk>
2019-12-19 12:23:35 +0600

    -a
    fix all

---
+++ src/main/java/dbService/SQLQueryBuilder.java
@@ -0,0 +1,17 @@
+package com.gmail.mrmioxin.kbemp.dbService;
+
+import java.util.Map;
+
+/**
+ * Created by palchuk on 27.06.2018.
+ */
+public class SQLQueryBuilder {
+    private Map<String,String> fields;
+    private SQLQueryBuilder() {};
+
+    public void from(String table) {};
+    public void where(String table) {};
+    public String build() {
+        return null;
+    };
+}
